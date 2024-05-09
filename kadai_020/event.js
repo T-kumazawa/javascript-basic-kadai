14d4879300772dd45e3bc19417e70f842c6c1772
T-kumazawa <166766489+T-kumazawa@users.noreply.github.com>
2024-05-10 08:33:04 +0900

    kadai_020をアップロード

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,9 +1,11 @@
 // btnというidを持つHTML要素を取得して、定数に代入する
-const btn = document.getElementById('btn');
+const button = document.getElementById('btn');
+
+// text要素を定数に代入する
+const text = document.getElementById('text');
 
 // HTML要素がクリックされたときにイベント処理を実行する
-document.getElementById('btn').addEventListener('click',function() {
-
-  document.getElementById('text').innerHTML = "ボタンをクリックしました";
+button.addEventListener('click',function() {
+   text.innerHTML = "ボタンをクリックしました";
 });
 
